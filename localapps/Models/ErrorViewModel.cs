d776400c6103d3ca4947bb79755b540a72bd716f
Akhil <akhilpphere@gmail.com>
2024-09-18 07:42:56 +0530

    second push

---
+++ localapps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace localapps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
